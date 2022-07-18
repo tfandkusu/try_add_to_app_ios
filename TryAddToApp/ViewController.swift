b4fdfe1a5d3ba9f778c13152a3b3bce35e26d656
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-07-18 23:28:35 +0900

    User can dismiss FlutterViewController

--- TryAddToApp/ViewController.swift
+++ TryAddToApp/ViewController.swift
@@ -8,15 +8,27 @@ class ViewController: UIViewController {
         let flutterViewController = FlutterViewController(
             engine: flutterEngine, nibName: nil, bundle: nil
         )
-        // flutterViewController.modalPresentationStyle = .fullScreen
+        flutterViewController.modalPresentationStyle = .fullScreen
+        let methodChannel = FlutterMethodChannel(
+            name: "com.tfandkusu.tryaddtoapp/call",
+            binaryMessenger: flutterViewController.binaryMessenger
+        )
+        methodChannel.setMethodCallHandler({ (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            if(call.method == "callNativePage") {
+                let secondViewController = SecondViewController()
+                flutterViewController.present(secondViewController, animated: true)
+                result(1);
+            }else if(call.method == "back") {
+                flutterViewController.dismiss(animated: true)
+                flutterEngine.viewController = nil
+                result(1);
+            }
+        })
         present(flutterViewController, animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
